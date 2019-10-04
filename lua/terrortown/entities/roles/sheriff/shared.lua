414600d28cec43ff29e7e5cb1f06b38446fc94f9
Alf Müller <4lf-mueller@gmx.de>
2019-10-04 16:41:27 +0200

    fix

--- lua/terrortown/entities/roles/sheriff/shared.lua
+++ lua/terrortown/entities/roles/sheriff/shared.lua
@@ -35,22 +35,22 @@ end)
 
 if CLIENT then
 	hook.Add("TTT2FinishedLoading", "SherInitD", function()
-			-- Role specific language elements
-			LANG.AddToLanguage("English", SHERIFF.name, "Sheriff")
-			LANG.AddToLanguage("English", "info_popup_" .. SHERIFF.name, [[You are the Sheriff! Try to get a mate to protect the innocents.]])
-			LANG.AddToLanguage("English", "body_found_" .. SHERIFF.abbr, "This person was a Sheriff!")
-			LANG.AddToLanguage("English", "search_role_" .. SHERIFF.abbr, "This person was a Sheriff!")
-			LANG.AddToLanguage("English", "target_" .. SHERIFF.name, "Sheriff")
-			LANG.AddToLanguage("English", "ttt2_desc_" .. SHERIFF.name, [[The Sheriff needs to protect the innocents with his deputy! If the Sheriff dies, the deputy will die too (automatically).]])
-			LANG.AddToLanguage("English", "credit_" .. SHERIFF.abbr .. "_all", "Sheriff, you have been awarded {num} equipment credit(s) for your performance.")
+		-- Role specific language elements
+		LANG.AddToLanguage("English", SHERIFF.name, "Sheriff")
+		LANG.AddToLanguage("English", "info_popup_" .. SHERIFF.name, [[You are the Sheriff! Try to get a mate to protect the innocents.]])
+		LANG.AddToLanguage("English", "body_found_" .. SHERIFF.abbr, "This person was a Sheriff!")
+		LANG.AddToLanguage("English", "search_role_" .. SHERIFF.abbr, "This person was a Sheriff!")
+		LANG.AddToLanguage("English", "target_" .. SHERIFF.name, "Sheriff")
+		LANG.AddToLanguage("English", "ttt2_desc_" .. SHERIFF.name, [[The Sheriff needs to protect the innocents with his deputy! If the Sheriff dies, the deputy will die too (automatically).]])
+		LANG.AddToLanguage("English", "credit_" .. SHERIFF.abbr .. "_all", "Sheriff, you have been awarded {num} equipment credit(s) for your performance.")
 
-			LANG.AddToLanguage("Deutsch", SHERIFF.name, "Sheriff")
-			LANG.AddToLanguage("Deutsch", "info_popup_" .. SHERIFF.name, [[Du bist ein Sheriff! Versuche, einen Deputy zu bekommen und so ie Innocents besser zu beschützen.]])
-			LANG.AddToLanguage("Deutsch", "body_found_" .. SHERIFF.abbr, "Er war ein Sheriff!")
-			LANG.AddToLanguage("Deutsch", "search_role_" .. SHERIFF.abbr, "Diese Person war ein Sheriff!")
-			LANG.AddToLanguage("Deutsch", "target_" .. SHERIFFSHERIFF.name, "Sheriff")
-			LANG.AddToLanguage("Deutsch", "ttt2_desc_" .. SHERIFF.name, [[Der Sheriff muss mit seinem Deputy die Unschuldigen beschützen! Wenn der Sheriff stirbt, wird auch der Hilfssheriff automatisch sterben.]])
-			LANG.AddToLanguage("Deutsch", "credit_" .. SHERIFF.abbr .. "_all", "Sheriffs, euch wurden {num} Ausrüstungs-Credit(s) für eure Leistung gegeben.")
+		LANG.AddToLanguage("Deutsch", SHERIFF.name, "Sheriff")
+		LANG.AddToLanguage("Deutsch", "info_popup_" .. SHERIFF.name, [[Du bist ein Sheriff! Versuche, einen Deputy zu bekommen und so ie Innocents besser zu beschützen.]])
+		LANG.AddToLanguage("Deutsch", "body_found_" .. SHERIFF.abbr, "Er war ein Sheriff!")
+		LANG.AddToLanguage("Deutsch", "search_role_" .. SHERIFF.abbr, "Diese Person war ein Sheriff!")
+		LANG.AddToLanguage("Deutsch", "target_" .. SHERIFF.name, "Sheriff")
+		LANG.AddToLanguage("Deutsch", "ttt2_desc_" .. SHERIFF.name, [[Der Sheriff muss mit seinem Deputy die Unschuldigen beschützen! Wenn der Sheriff stirbt, wird auch der Hilfssheriff automatisch sterben.]])
+		LANG.AddToLanguage("Deutsch", "credit_" .. SHERIFF.abbr .. "_all", "Sheriffs, euch wurden {num} Ausrüstungs-Credit(s) für eure Leistung gegeben.")
 	end)
 end
 
