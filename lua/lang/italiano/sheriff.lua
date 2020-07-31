0579760e8c1a11b77b7a4e44fa60e7f22f0d217f
ThePlatinumGhost <36534723+ThePlatinumGhost@users.noreply.github.com>
2020-07-31 17:21:54 +0200

    Update Sheriff (#2)

--- lua/lang/italiano/sheriff.lua
+++ lua/lang/italiano/sheriff.lua
@@ -9,8 +9,8 @@ L["target_" .. SHERIFF.name] = "Sceriffo"
 L["ttt2_desc_" .. SHERIFF.name] = [[Lo Sceriffo deve proteggere gli innocenti con il suo Vice! Se lo Sceriffo muore, anche il Vice lo farà (automaticamente).]]
 L["credit_" .. SHERIFF.abbr .. "_all"] = "Sceriffo, ti è stato dato {num} credito per la tua performance."
 
-L[DEPUTY.name] = "Vice"
-L["target_" .. DEPUTY.name] = "Vice"
-L["ttt2_desc_" .. DEPUTY.name] = [[Devi aiutare a proteggere gli innocenti!]]
-L["body_found_" .. DEPUTY.abbr] = "Era un Vice..."
-L["search_role_" .. DEPUTY.abbr] = "Questa persona era un Vice!"
+L[DEPUTY.name] = "Deputato"
+L["target_" .. DEPUTY.name] = "Deputato"
+L["ttt2_desc_" .. DEPUTY.name] = [[Devi aiutare lo Sceriffo a proteggere gli innocenti!]]
+L["body_found_" .. DEPUTY.abbr] = "Era un Deputato..."
+L["search_role_" .. DEPUTY.abbr] = "Questa persona era un Deputato!"
